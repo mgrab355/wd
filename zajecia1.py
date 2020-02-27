078fc2c8b7a3d16d030c5fcfbbf599a9559d0e17
sylwia12345678 <sylwia.anna.u@gmail.com>
2020-02-27 10:21:12 +0100

    commint3

--- zajecia1.py
+++ zajecia1.py
@@ -1,3 +1,26 @@
-liczba=0
-print(liczba)
-print("ala")
+# liczba = 0
+# print(liczba)
+# print(liczba+liczba)
+# print("ala")
+# def funkcja():
+#     liczba = 0
+#     print(liczba)
+#     print(liczba+liczba)
+#     print("ala")
+
+print("ala ma " + "5 lat ") #konkatencja
+print(0 + 1)
+print(2 - 1)
+print(2 * 1)
+print(2 // 1) #dzielenie bez reszty
+print(2 ** 1) #potegowanie
+print(2 % 1) #dzielenie modulo 
+print(5)
+print("Ala ma 5 lat")
+# print("ala ma "+ 5 +"lat") blad
+print("ala ma " +str(5) + " lat")
+
+#formatowanie wejscia
+
+print("ala ma {} lat".format(5))
+print("ala ma {1} lat a marta {0}".format(5,10))
